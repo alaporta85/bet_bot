702d1f4db1bdc10d55aebafbdefb67b22be3ce58
alaporta85 <ndr.lpr@gmail.com>
2022-04-15 11:05:33 +0200

    trying updating db all at once

---
+++ prova.py
@@ -0,0 +1,45 @@
+import time
+import db_functions as dbf
+
+
+def db_insertmany(table: str, values: list):
+
+	"""
+	Insert a new row in the table.
+	"""
+
+	db, c = dbf.start_db()
+
+	vals = ', '.join(['?' for _ in values[0]])
+	query = f'INSERT INTO {table} VALUES ({vals})'
+
+	c.executemany(query, values)
+	db.commit()
+	db.close()
+
+
+def one_by_one(n):
+	for _ in range(n):
+		dbf.db_insert(table='prova',
+					  columns=['Field1_int', 'Field2_str',
+							   'Field3_float', 'Field4_str'],
+					  values=[5, 'aaa', .5, 'bbb'])
+
+
+def all_at_once(n):
+	values = [(5, 'aaa', .5, 'bbb') for _ in range(n)]
+	db_insertmany(table='prova',
+				  values=values)
+
+
+N = 1000
+
+dbf.empty_table('prova')
+t0 = time.time()
+one_by_one(N)
+print('One by one:', time.time() - t0)
+
+dbf.empty_table('prova')
+t0 = time.time()
+all_at_once(N)
+print('All at once:', time.time() - t0)
